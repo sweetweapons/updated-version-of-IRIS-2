643c9cdc0e07b712f5becc5433b3131d041ef9ea
akash <akashrajendraprasad73@gmail.com>
2022-10-07 15:38:31 +0530

    test percentage changed1

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ labels ={
 
 #split the data set
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.50)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.30)
 
 #Using decision tree algorithm
 from sklearn import tree
